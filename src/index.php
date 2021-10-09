fc7a45256cd79c47c63e0e23f3fc38c5269687d4
SteveSerjeant <steven.serjeant@students.plymouth.ac.uk>
2021-10-09 16:05:05 +0100

    Initial project upload of one page

---
+++ src/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <title>Security Dashboard</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" href="../css/stylesheet.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+<br>
+<h1 class="h1">This is the start of the project</h1>
+<!--<div class="container-fluid">-->
+<!--    <h1 class="h1">Responsive Columns</h1>-->
+<!--    <div class="row">-->
+<!--        <div class="col-sm-3" style="background-color:lavender;">.col-sm-3</div>-->
+<!--        <div class="col-sm-3" style="background-color: lavenderblush;">.col-sm-3</div>-->
+<!--        <div class="col-sm-3" style="background-color:lavender;">.col-sm-3</div>-->
+<!--        <div class="col-sm-3" style="background-color: lavenderblush;">.col-sm-3</div>-->
+<!--    </div>-->
+<!---->
+<!--</div>-->
+
+
+</body>
+
+</html>
+
+<?php
