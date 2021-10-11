3289d7d6d904e8047aeb093ff5d6059c2bd25e12
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2021-10-11 18:45:12 +0100

    Commit 21 10 11
    Restart of project using github desktop and saving project to the ampps.www folder to demonstrate pages using localhost. Also added initial basic stylesheet to centre the text.

--- src/index.php
+++ src/index.php
@@ -5,14 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel = "stylesheet" href="../css/stylesheet.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">-->
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
+<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>-->
+<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>-->
 </head>
 <body>
 <br>
 <h1 class="h1">This is the start of the project</h1>
+<br>
+<h1 class="h1">Security Dashboard for Home Users</h1>
 <!--<div class="container-fluid">-->
 <!--    <h1 class="h1">Responsive Columns</h1>-->
 <!--    <div class="row">-->
