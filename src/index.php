8b1d2a1f19ae17bf486223485aace33987453b19
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2021-10-23 23:27:23 +0100

    start of sprint 1
    Start of sprint 1 - beginning of login page with stylesheet

--- src/index.php
+++ src/index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang = "en">
 <head>
-    <title>* Security Dashboard*</title>
+    <title>Security Dashboard</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel = "stylesheet" href="../css/stylesheet.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+<!--    <link rel="stylesheet" href="../css/stylesheet.css" type="text/css">-->
+    <link href="style.css" rel="stylesheet" type="text/css">
+<!--    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">-->
+<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">-->
 <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">-->
 <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>-->
 <!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>-->
@@ -17,20 +21,27 @@
 <h2 class="h1">COMP3000 2121/2022</h2>
 <br>
 <h3 class="h1">Security Dashboard for Home Users</h3>
-<!--<div class="container-fluid">-->
-<!--    <h1 class="h1">Responsive Columns</h1>-->
-<!--    <div class="row">-->
-<!--        <div class="col-sm-3" style="background-color:lavender;">.col-sm-3</div>-->
-<!--        <div class="col-sm-3" style="background-color: lavenderblush;">.col-sm-3</div>-->
-<!--        <div class="col-sm-3" style="background-color:lavender;">.col-sm-3</div>-->
-<!--        <div class="col-sm-3" style="background-color: lavenderblush;">.col-sm-3</div>-->
-<!--    </div>-->
-<!---->
-<!--</div>-->
+
+
+
+<div class="login">
+    <h1>Dashboard Login</h1>
+    <form action="authenticate.php" method="post">
+        <label for="username">
+            <i class="fas fa-user"></i>
+        </label>
+        <input type="text" name="username" placeholder="Username" id="username" required>
+        <label for="password">
+            <i class="fas fa-lock"></i>
+        </label>
+        <input type="password" name="password" placeholder="Password" id="password" required>
+        <input type="submit" value="Login">
+    </form>
+</div>
 
 
 </body>
 
 </html>
 
-<?php
+
