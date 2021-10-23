63685ea6a30b1cfa1e05935da4e73eb24e87b8f8
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2021-10-23 20:45:10 +0100

    Update index.php
    testing if changes go to new development branch

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang = "en">
 <head>
-    <title>Security Dashboard</title>
+    <title>* Security Dashboard*</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel = "stylesheet" href="../css/stylesheet.css">
